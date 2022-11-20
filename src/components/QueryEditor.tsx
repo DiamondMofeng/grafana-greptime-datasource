6c1631987bfb9b4447e9d0369be73b0d846c2cd8
Mofeng <363034937@qq.com>
2022-11-20 22:49:20 +0800

    🎈 perf(QueryEditor): Replaced the query input with @Grafana/ui/CodeEditor

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -1,48 +1,48 @@
 import defaults from 'lodash/defaults';
 
 import React, { ChangeEvent, PureComponent } from 'react';
-import { LegacyForms } from '@grafana/ui';
+import { CodeEditor, monacoTypes } from '@grafana/ui';
 import { QueryEditorProps } from '@grafana/data';
 import { DataSource } from '../datasource';
 import { defaultQuery, MyDataSourceOptions, MyQuery } from '../types';
 
-const { FormField } = LegacyForms;
-
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
 export class QueryEditor extends PureComponent<Props> {
-  onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onChange, query } = this.props;
-    onChange({ ...query, queryText: event.target.value });
+  getEditorValue: any | undefined;
+  editor: monacoTypes.editor.IStandaloneCodeEditor | undefined;
+
+  changeQuery = (key: keyof MyQuery, value: any) => {
+    const { query, onChange } = this.props;
+    onChange({ ...query, [key]: value });
   };
 
-  onConstantChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onChange, query, onRunQuery } = this.props;
-    onChange({ ...query, constant: parseFloat(event.target.value) });
-    // executes the query
-    onRunQuery();
+  onChangeFactory = (key: keyof MyQuery) => (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    this.changeQuery(key, event.target.value);
+  };
+
+  onEditorDidMount = (editor: monacoTypes.editor.IStandaloneCodeEditor) => {
+    this.editor = editor;
+  };
+
+  onRawQueryChange = () => {
+    if (!this.editor) {
+      return;
+    }
+    this.changeQuery('queryText', this.editor.getValue());
   };
 
   render() {
     const query = defaults(this.props.query, defaultQuery);
-    const { queryText, constant } = query;
 
     return (
-      <div className="gf-form">
-        <FormField
-          width={4}
-          value={constant}
-          onChange={this.onConstantChange}
-          label="Constant"
-          type="number"
-          step="0.1"
-        />
-        <FormField
-          labelWidth={8}
-          value={queryText || ''}
-          onChange={this.onQueryTextChange}
-          label="Query Text"
-          tooltip="Not used yet"
+      <div>
+        <CodeEditor
+          height={'200px'}
+          language="sql"
+          value={query.queryText}
+          onEditorDidMount={this.onEditorDidMount}
+          onBlur={this.onRawQueryChange}
         />
       </div>
     );
--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,11 @@
 import { DataQuery, DataSourceJsonData } from '@grafana/data';
 
 export interface MyQuery extends DataQuery {
-  queryText?: string;
-  constant: number;
+  queryText: string;
 }
 
 export const defaultQuery: Partial<MyQuery> = {
-  // constant: 6.5,
+  queryText: 'SELECT * FROM numbers LIMIT 5',
 };
 
 /**
